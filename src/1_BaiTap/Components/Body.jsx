8e0b6e1c5f43cedbafa0e49b3e871107eb36494d
minhtruyen <ht.minhtruyen.dev@gmail.com>
2022-09-27 12:51:40 +0700

    upload 1

---
+++ src/1_BaiTap/Components/Body.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from './Banner'
+import ListItem from './ListItem'
+
+const Body = () => {
+    return (
+        <div>
+            <Banner />
+            <ListItem />
+        </div>
+    )
+}
+
+export default Body
